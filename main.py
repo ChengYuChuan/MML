a684fe410692f42e9abac11bbc71dde33b8df377
Max-Hi <levin.hirsch@web.de>
2023-10-29 18:16:36 +0100

    bug fix saving to folder

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import os
 
 # argparser:
 parser = argparse.ArgumentParser(
@@ -132,7 +133,7 @@ def trainAndVisualize(depth: int, width: int, initDistr: str, actFunc: str):
         plt.xlabel("weight value")
         plt.ylabel("value probability")
     # plt.show()
-    plt.savefig(f"images/Weight_Distribution_depth{args.depth}_width{args.width}_init={args.initialisation}_act={args.activation}.png")
+    plt.savefig(os.path.join(output_dir, f"Weight_Distribution_depth{args.depth}_width{args.width}_init={args.initialisation}_act={args.activation}.png"))
 
 LR = float(args.learningrate)
 EPOCHS = int(args.epochs)
